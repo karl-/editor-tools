83132bde20d2f742403c16f306521f9e45dd0307
Karl Henkel <karlh07@gmail.com>
2018-06-23 13:39:59 -0400

    initial commit

---
+++ Editor/OpenPackageManifest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace UnityEngine.ProBuilder.Debug.Editor
+{
+	static class OpenPackageManifest
+	{
+		[MenuItem("Assets/Debug/Open Package Manifest", false, 0)]
+		static void MenuOpenPackageManifest()
+		{
+			SublimeEditor.Open("Packages/manifest.json");
+		}
+	}
+}
