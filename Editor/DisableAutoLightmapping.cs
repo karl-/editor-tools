059abf8937e36a408bfb320a334b0be8f4b075e4
Karl Henkel <karlh@unity3d.com>
2018-07-02 10:12:31 -0400

    remove dummy script, add DisableAutoLightmapping util

---
+++ Editor/DisableAutoLightmapping.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Karl.Editor
+{
+	[InitializeOnLoad]
+	static class DisableAutoLightmapping
+	{
+		static DisableAutoLightmapping()
+		{
+			if (Lightmapping.giWorkflowMode != Lightmapping.GIWorkflowMode.Legacy)
+				Lightmapping.giWorkflowMode = Lightmapping.GIWorkflowMode.OnDemand;
+		}
+	}
+}
